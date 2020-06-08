9337645882d626eab54463a0dbe28d3821ff96e7
LuisGarGomez <luisenquegarciagomez@ciencias.unam.mx>
2020-06-08 14:43:51 -0500

    Mas cambios

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Rama Luis Nuevos cambios");
+        System.out.println("Cambios Luis");
     }
 }
